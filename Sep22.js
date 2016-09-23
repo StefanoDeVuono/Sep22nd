30929ade8043945881fcb05f351418b6a6f60919
yagmur <yagmuruyanik@gmail.com>
2016-09-22 19:52:59 -0700

    Add console.log for html to javascript file

--- Sep22.js
+++ Sep22.js
@@ -1,1 +1,15 @@
-var Name = ["Yagmur", "Mayowa"];
+//Create a name array
+var Name = ["Yagmur", "Mayowa", "Ope", "Ore"];
+
+//Console.log for html
+var output = document.getElementById("Name");
+output.innerHTML = "hello world";
+
+
+//Print Name loop
+for (var i = 0; i < 9; i++) {
+   console.log(i);
+   // more statements
+}
+
+$()
